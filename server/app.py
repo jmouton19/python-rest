8055f3a520206650a428304ab14d33d0f18baad7
JC Mouton <22549889@sun.ac.za>
2022-02-23 02:15:40 +0200

    backend branch test

--- server/app.py
+++ server/app.py
@@ -1,24 +1,14 @@
 from flask import Flask, send_from_directory
-from flask_cors import CORS, cross_origin
-import os
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__,static_folder='../client/build',static_url_path='')
-
-cors = CORS(app)
-
-@app.route('/api/')
-@cross_origin()
-def Welcome():
-    return "Welcome to the API!!!"
-
-@app.route('/api/getData/')
-@cross_origin()
-def SampleData():
-    return {"Subject": "Computer Science", "Module": "CS334"}
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
+def test():
+    return "<h1>Testing</h1>"
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
