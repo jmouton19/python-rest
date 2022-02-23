786474b4aeb0357a9582c40b9a5d9f605c34224f
JC Mouton <22549889@sun.ac.za>
2022-02-23 19:05:09 +0200

    heroku

--- server/app.py
+++ server/app.py
@@ -1,11 +1,24 @@
 from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-db = SQLAlchemy(app)
-
+#######################################################################
+app = Flask(__name__,static_folder='../client/build',static_url_path='')
 @app.route('/')
+def server():
+    return send_from_directory(app.static_folder, 'index.html')
+#######################################################################
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost/students'
+db=SQLAlchemy(app)
+
+
+@app.route('/api/getData/')
+def SampleData():
+    return {'name':'Nicol','passowrd':"123"}
+
+@app.route('/test')
 def test():
     return "<h1>Testing</h1>"
 
