b0f2ee6663f132b3e05c5b11e438f86e9fc79883
nicolvisser <nicolvisser@yahoo.com>
2022-02-23 18:33:52 +0200

    Worked on SignUp Component and added App Bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AppBar from "./components/AppBar/AppBar";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
 	return (
-		<Router>
-			<Routes>
-				<Route path="/signup" element={<SignUp />} />
-				<Route path="/" element={<Home />} />
-			</Routes>
-		</Router>
+		<React.Fragment>
+			<AppBar />
+			<Router>
+				<Routes>
+					<Route path="/signup" element={<SignUp />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</Router>
+		</React.Fragment>
 	);
 }
 
