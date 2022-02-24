2b768273df663d54dc796bfa746bb9f61336058d
JC Mouton <22549889@sun.ac.za>
2022-02-24 18:30:25 +0200

    database on heroku

--- server/app.py
+++ server/app.py
@@ -9,7 +9,7 @@ def server():
     return send_from_directory(app.static_folder, 'index.html')
 #######################################################################
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:qwerty4862@localhost:5432/group8'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://tqafvqemczrnoc:3505179c6cbe8351cd47495888ef496014877341e918f6dc9e76e2a1e342535f@ec2-3-220-214-162.compute-1.amazonaws.com:5432/dd2jk9t71796tm'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db=SQLAlchemy(app)
 
