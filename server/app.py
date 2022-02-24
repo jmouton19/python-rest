2449d48d0c676cd3fc76bdc8cd47582fc4e05033
JC Mouton <22549889@sun.ac.za>
2022-02-24 02:25:12 +0200

    database setup and user checking

--- server/app.py
+++ server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_from_directory
+from crypt import methods
+from flask import Flask, send_from_directory, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 #######################################################################
@@ -8,8 +9,17 @@ def server():
     return send_from_directory(app.static_folder, 'index.html')
 #######################################################################
 
-app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost/students'
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:qwerty4862@localhost:5432/group8'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db=SQLAlchemy(app)
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String(32),unique=True)
+    password=db.Column(db.String(32))
+    def __init__(self,email,password):
+        self.email=email
+        self.password=password
 
 
 @app.route('/api/getData/')
@@ -20,5 +30,19 @@ def SampleData():
 def test():
     return "<h1>Testing</h1>"
 
+@app.route('/user=<usr>',methods=['GET'])
+def user(usr):
+    result=db.session.query(User).filter(User.email==usr).one_or_none()
+    if result:
+        print(result.email)
+        return jsonify(email=result.email,
+                        password=result.password)
+    else:
+        print('User not found')
+        return jsonify(email=None,
+                        password=None)
+
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
