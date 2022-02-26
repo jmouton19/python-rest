e6e5b6ab7e9c69acf910b022894882f75f6fea5e
nicolvisser <nicolvisser@yahoo.com>
2022-02-26 13:23:26 +0200

    Removed test data call to api from home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,33 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Home.css";
-import Button from "@mui/material/Button";
 
 function Home() {
-	const [data, setData] = useState({});
-
-	const requestURL = "/api/getData/";
-
-	function getData() {
-		fetch(requestURL)
-			.then((res) => res.json())
-			.then(
-				(data) => {
-					console.log(data);
-					setData(data);
-				},
-				(error) => {
-					console.log(error);
-				}
-			);
-	}
 	return (
 		<React.Fragment className="App">
-			<header className="App-header">
-				<Button variant="contained" onClick={getData}>
-                    Get Test Data from Back End
-				</Button>
-				<p>{JSON.stringify(data)}</p>
-			</header>
+			<header className="App-header"></header>
 		</React.Fragment>
 	);
 }
