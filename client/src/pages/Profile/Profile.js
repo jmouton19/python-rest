dcdfc4f950281731176966a6c82b0f462944e282
Phillip <schommarzp@gmail.com>
2022-02-28 11:06:27 +0200

    Added Dialog for edit

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -3,10 +3,17 @@ import { useUserCredentials } from "../../AuthProvider";
 import Grid from "@mui/material/Grid";
 import {
 	Avatar,
+	Button,
 	Container,
+	Dialog,
+	DialogActions,
+	DialogTitle,
 	Fab,
 	Paper,
 	Stack,
+	Step,
+	Stepper,
+	StepButton,
 	TextField,
 	Typography,
 } from "@mui/material";
@@ -24,7 +31,20 @@ const fabStyle = {
 
 function Profile() {
 	const user = useUserCredentials();
-	const [editable, setEditable] = useState(false);
+	const [edit, setEdit] = useState(false);
+	const [activeStep, setActiveStep] = useState(1);
+
+	const handleStep = (step) => () => {
+		setActiveStep(step);
+	};
+
+	const handleEditOpen = () => {
+		setEdit(true);
+	};
+
+	const handleEditClose = () => {
+		setEdit(false);
+	};
 
 	return (
 		<React.Fragment>
@@ -32,107 +52,106 @@ function Profile() {
 				style={fabStyle}
 				color="primary"
 				aria-label="edit"
-				onClick={() => setEditable(!editable)}
+				onClick={handleEditOpen}
 			>
 				<EditIcon />
 			</Fab>
-			<Container maxWidth="md">
-				<Grid container padding={1} rowSpacing={3} alignItems="center">
-					{editable ? ( //Editable layout TODO: change actual values on save
-						<Grid item xs={12}>
-							<Paper elevation={4}>
-								<Grid container alignItems="center" padding={2}>
-									<Grid item xs={3}>
+			<Dialog open={edit} onClose={handleEditClose}>
+				<DialogTitle>Edit Details</DialogTitle>
+				<Stepper nonLinear activeStep={activeStep}>
+					<Step>
+						<StepButton color="inherit" onClick={handleStep(1)}>
+							General Info
+						</StepButton>
+						<Grid container padding={1} rowSpacing={3} alignItems="center">
+							<Grid item xs={12}>
+								<Grid container alignItems="center" padding={1} spacing={1}>
+									<Grid item xs={12}>
 										<Stack>
-											<Avatar
-												sx={{ width: 100, height: 100 }}
-												src={user.avatarUrl}
-											></Avatar>
 											<AvatarPicker
-												hideAvatar={true}
 												setAvatarUrl={(imageUrl) => user.setAvatarUrl(imageUrl)}
 											/>
 										</Stack>
 									</Grid>
-									<Grid item xs={9} spacing={8}>
+									<Grid item xs={12}>
 										{user.userType === "developer" ? (
-											<Stack>
-												<Stack direction="row">
-													<TextField
-														variant="standard"
-														defaultValue={user.firstName}
-														inputProps={{ style: { fontSize: 48 } }}
-														size="small"
-													/>
-													<TextField
-														variant="standard"
-														defaultValue={user.lastName}
-														inputProps={{ style: { fontSize: 48 } }}
-														size="small"
-													/>
-												</Stack>
+											<Stack spacing={1}>
 												<TextField
-													variant="standard"
+													variant="outlined"
+													defaultValue={user.firstName}
+												/>
+												<TextField
+													variant="outlined"
+													defaultValue={user.lastName}
+												/>
+												<TextField
+													variant="outlined"
 													defaultValue={user.username}
-													inputProps={{ style: { fontSize: 25 } }}
 												/>
-												<TextField
-													variant="standard"
-													defaultValue={user.email}
-													inputProps={{ style: { fontSize: 20 } }}
-												/>
+												<TextField variant="outlined" defaultValue={user.email} />
 											</Stack>
 										) : (
 											<Stack>
 												<TextField
-													variant="standard"
+													variant="outlined"
 													defaultValue={user.username}
-													inputProps={{ style: { fontSize: 48 } }}
-													size="small"
 												/>
-												<TextField
-													variant="standard"
-													defaultValue={user.email}
-													inputProps={{ style: { fontSize: 20 } }}
-												/>
+												<TextField variant="outlined" defaultValue={user.email} />
 											</Stack>
 										)}
 									</Grid>
 								</Grid>
-							</Paper>
+								<DialogActions>
+									<Button 
+										onClick={handleEditClose} 
+										variant="contained"
+										//TODO:Add server communication and updating
+									>
+										Save Changes
+									</Button>
+								</DialogActions>
+							</Grid>
 						</Grid>
-					) : ( //Non-edit profile version
-						<Grid item xs={12}>
-							<Paper elevation={4}>
-								<Grid container alignItems="center" padding={2}>
-									<Grid item xs={3}>
-										<Avatar
-											sx={{ width: 100, height: 100 }}
-											src={user.avatarUrl}
-										></Avatar>
-									</Grid>
-									<Grid item xs={9} spacing={8}>
-										<Stack>
-											{user.userType === "developer" ? (
-												<div>
-													<Typography variant="h3">
-														{user.firstName} {user.lastName}
-													</Typography>
-													<Typography variant="h5">{user.username}</Typography>
-													<Typography variant="h6">{user.email}</Typography>
-												</div>
-											) : (
-												<div>
-													<Typography variant="h3">{user.username}</Typography>
-													<Typography variant="h6">{user.email}</Typography>
-												</div>
-											)}
-										</Stack>
-									</Grid>
+					</Step>
+					<Step>
+						<StepButton color="inherit" onClick={handleStep(2)}>
+							Additional Information
+						</StepButton>
+					</Step>
+				</Stepper>
+			</Dialog>
+			<Container maxWidth="md">
+				<Grid container padding={1} rowSpacing={3} alignItems="center">
+					<Grid item xs={12}>
+						<Paper elevation={4}>
+							<Grid container alignItems="center" padding={2}>
+								<Grid item xs={3}>
+									<Avatar
+										sx={{ width: 100, height: 100 }}
+										src={user.avatarUrl}
+									></Avatar>
 								</Grid>
-							</Paper>
-						</Grid>
-					)}
+								<Grid item xs={9} spacing={8}>
+									<Stack>
+										{user.userType === "developer" ? (
+											<div>
+												<Typography variant="h3">
+													{user.firstName} {user.lastName}
+												</Typography>
+												<Typography variant="h5">{user.username}</Typography>
+												<Typography variant="h6">{user.email}</Typography>
+											</div>
+										) : (
+											<div>
+												<Typography variant="h3">{user.username}</Typography>
+												<Typography variant="h6">{user.email}</Typography>
+											</div>
+										)}
+									</Stack>
+								</Grid>
+							</Grid>
+						</Paper>
+					</Grid>
 				</Grid>
 			</Container>
 		</React.Fragment>
