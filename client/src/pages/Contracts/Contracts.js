75201e9938ab91d44947e6a30088e743b19ca05c
nicolvisser <nicolvisser@yahoo.com>
2022-02-28 16:21:04 +0200

    Added contracts page with sample data

--- client/src/pages/Contracts/Contracts.js
+++ client/src/pages/Contracts/Contracts.js
@@ -1,8 +1,54 @@
-import React from "react";
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+
+import { sampleData } from "./generateSampleData";
+
+console.log(sampleData);
 
 function Contracts() {
 	return (
-		<div>Contracts</div>
+		<List sx={{ width: "100%", bgcolor: "background.paper" }}>
+			{sampleData.map((dataItem) => (
+				<React.Fragment key={dataItem.contract_id}>
+					<ListItem alignItems="flex-start">
+						<ListItemAvatar>
+							<Avatar alt={dataItem.company_name} />
+						</ListItemAvatar>
+						<ListItemText
+							primary={dataItem.company_name}
+							secondary={
+								<React.Fragment>
+									<Typography
+										component="span"
+										variant="body2"
+										color="text.primary"
+									>
+										{`Duration: ${dataItem.length} Months`}
+									</Typography>
+									<br />
+									<Typography
+										component="span"
+										variant="body2"
+										color="text.primary"
+									>
+										{`Value: $${dataItem.value}`}
+									</Typography>
+									<br />
+									{dataItem.description}
+								</React.Fragment>
+							}
+						/>
+					</ListItem>
+					<Divider variant="inset" component="li" />
+				</React.Fragment>
+			))}
+		</List>
 	);
 }
 
