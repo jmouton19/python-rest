a5609aba4a05f760994b049a692b0d1c1936cea0
JC Mouton <22549889@sun.ac.za>
2022-03-07 15:10:47 +0200

    bulk delete devs/companies

--- server/views/company.py
+++ server/views/company.py
@@ -4,31 +4,36 @@ from server.models import db, Company
 from server.views.checkemail import email_checkr
 from argon2 import PasswordHasher
 
-@app.route('/api/company', methods=['POST'])
+@app.route('/api/company', methods=['POST','DELETE'])
 def signup_company():
-    request_data = request.get_json()
-    email=request_data['email']
-    company_name=request_data['company_name']
-    company=db.session.query(Company).filter(Company.company_name==company_name).one_or_none()
-    checker=email_checkr(email)
-    if checker['success']==True:
-        if company==None:
-            ph = PasswordHasher()
-            hashed = ph.hash(request_data['password'])
-            new_company=Company(
-                password=hashed,
-                company_name=company_name,
-                industry=request_data['industry'],
-                avatar=request_data['avatar'],
-                email=email,
-            )
-            db.session.add(new_company)
-            db.session.commit()
+    if request.method=='POST':
+        request_data = request.get_json()
+        email=request_data['email']
+        company_name=request_data['company_name']
+        company=db.session.query(Company).filter(Company.company_name==company_name).one_or_none()
+        checker=email_checkr(email)
+        if checker['success']==True:
+            if company==None:
+                ph = PasswordHasher()
+                hashed = ph.hash(request_data['password'])
+                new_company=Company(
+                    password=hashed,
+                    company_name=company_name,
+                    industry=request_data['industry'],
+                    avatar=request_data['avatar'],
+                    email=email,
+                )
+                db.session.add(new_company)
+                db.session.commit()
+            else:
+                return jsonify(success=False,message="Company with this name already exists")
         else:
-            return jsonify(success=False,message="Company with this name already exists")
-    else:
-         return jsonify(checker)
-    return jsonify(success=True,message="Company has been registered")
+            return jsonify(checker)
+        return jsonify(success=True,message="Company has been registered")
+    elif request.method=='DELETE':
+        db.session.query(Company).delete()
+        db.session.commit()
+        return jsonify(success=True,message="All companies deleted")
 
 @app.route('/api/company/<company>', methods=['GET','DELETE'])
 def check_company_name(company):
--- server/views/developer.py
+++ server/views/developer.py
@@ -1,78 +1,84 @@
+from operator import methodcaller
 from flask import jsonify, request
 from server import app
 from server.models import Contract, db, Developer, DeveloperLanguages, Application, Company, BlockedCompany
 from server.views.checkemail import email_checkr
 from argon2 import PasswordHasher
 
-@app.route('/api/developer', methods=['POST'])
+@app.route('/api/developer', methods=['POST,DELETE'])
 def signup_developer():
-    request_data = request.get_json()
-    username=request_data['username']
-    email=request_data['email']
-    developer=db.session.query(Developer).filter(Developer.username==username).one_or_none()
-    checker=email_checkr(email)
-    if checker['success']==True:
-        if developer==None:
-            ph = PasswordHasher()
-            hashed = ph.hash(request_data['password'])
-            new_dev=Developer(
-            password=hashed,
-            username=username,
-            name=request_data['name'],
-            surname=request_data['surname'],
-            avatar=request_data['avatar'],
-            email=email,
-            )
-            dev_languages=request_data['developer_languages']
-            new_dev_languages=DeveloperLanguages()
-            for key, value in dev_languages.items():
-                match key:
-                    case 'C':
-                        new_dev_languages.c=value
-                    case 'C++':
-                        new_dev_languages.c_plusplus=value
-                    case 'Go':        
-                        new_dev_languages.go=value
-                    case 'Java':
-                        new_dev_languages.java=value
-                    case 'JavaScript':
-                        new_dev_languages.javascript=value
-                    case 'Kotlin':
-                        new_dev_languages.kotlin=value
-                    case 'Lua':
-                        new_dev_languages.lua=value
-                    case 'MATLAB':        
-                        new_dev_languages.matlab=value
-                    case 'Objective-C':
-                        new_dev_languages.objective_c=value
-                    case 'Perl':
-                        new_dev_languages.perl=value
-                    case 'Python':        
-                        new_dev_languages.python=value
-                    case 'PHP':        
-                        new_dev_languages.php=value
-                    case 'Rust':
-                        new_dev_languages.rust=value
-                    case 'Swift':
-                        new_dev_languages.swift=value
-                    case 'VBA':        
-                        new_dev_languages.vba=value
-                    case 'C#':
-                        new_dev_languages.c_sharp=value
-                    case 'TypeScript':
-                        new_dev_languages.typescript=value
-                    case 'Ruby':        
-                        new_dev_languages.ruby=value
-                    case 'R':        
-                        new_dev_languages.r=value
-            new_dev.developer_languages=new_dev_languages
-            db.session.add(new_dev)
-            db.session.commit()
+    if request.method=='POST':
+        request_data = request.get_json()
+        username=request_data['username']
+        email=request_data['email']
+        developer=db.session.query(Developer).filter(Developer.username==username).one_or_none()
+        checker=email_checkr(email)
+        if checker['success']==True:
+            if developer==None:
+                ph = PasswordHasher()
+                hashed = ph.hash(request_data['password'])
+                new_dev=Developer(
+                password=hashed,
+                username=username,
+                name=request_data['name'],
+                surname=request_data['surname'],
+                avatar=request_data['avatar'],
+                email=email,
+                )
+                dev_languages=request_data['developer_languages']
+                new_dev_languages=DeveloperLanguages()
+                for key, value in dev_languages.items():
+                    match key:
+                        case 'C':
+                            new_dev_languages.c=value
+                        case 'C++':
+                            new_dev_languages.c_plusplus=value
+                        case 'Go':        
+                            new_dev_languages.go=value
+                        case 'Java':
+                            new_dev_languages.java=value
+                        case 'JavaScript':
+                            new_dev_languages.javascript=value
+                        case 'Kotlin':
+                            new_dev_languages.kotlin=value
+                        case 'Lua':
+                            new_dev_languages.lua=value
+                        case 'MATLAB':        
+                            new_dev_languages.matlab=value
+                        case 'Objective-C':
+                            new_dev_languages.objective_c=value
+                        case 'Perl':
+                            new_dev_languages.perl=value
+                        case 'Python':        
+                            new_dev_languages.python=value
+                        case 'PHP':        
+                            new_dev_languages.php=value
+                        case 'Rust':
+                            new_dev_languages.rust=value
+                        case 'Swift':
+                            new_dev_languages.swift=value
+                        case 'VBA':        
+                            new_dev_languages.vba=value
+                        case 'C#':
+                            new_dev_languages.c_sharp=value
+                        case 'TypeScript':
+                            new_dev_languages.typescript=value
+                        case 'Ruby':        
+                            new_dev_languages.ruby=value
+                        case 'R':        
+                            new_dev_languages.r=value
+                new_dev.developer_languages=new_dev_languages
+                db.session.add(new_dev)
+                db.session.commit()
+            else:
+                return jsonify(success=False,message="Developer with this name already exists")
         else:
-            return jsonify(success=False,message="Developer with this name already exists")
-    else:
-        return jsonify(checker)
-    return jsonify(success=True,message="Developer has been registered")
+            return jsonify(checker)
+        return jsonify(success=True,message="Developer has been registered")
+    elif request.method=='DELETE':
+        db.session.query(Developer).delete()
+        db.session.commit()
+        return jsonify(success=True,message="All developers deleted")
 
 @app.route('/api/developer/<username>', methods=['GET','DELETE'])
 def check_username(username):
