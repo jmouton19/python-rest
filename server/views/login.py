18e26aff0250e736f3835783be9583f7963df116
JC Mouton <22549889@sun.ac.za>
2022-03-07 15:32:29 +0200

    catch mismatch errors

--- server/views/login.py
+++ server/views/login.py
@@ -1,3 +1,4 @@
+from ast import Try
 from flask import jsonify, request
 from server import app
 from server.views.checkemail import email_checkr
@@ -16,6 +17,8 @@ def login():
             name=result.company_name 
     else:
         return jsonify(success=False,message="This email has not been registered")
-    if ph.verify(result.password,request_data['password']):
-         return jsonify(success=True,message="Logged in",type=type,name=name)
-    return jsonify(success=False,message="Login failed")
+    try:
+        if ph.verify(result.password,request_data['password']):
+            return jsonify(success=True,message="Logged in",type=type,name=name)
+    except:
+        return jsonify(success=False,message="Login failed")
