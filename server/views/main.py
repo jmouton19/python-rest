dffb76a37bbfd88940585e678038623fd2596dfc
nicolvisser <nicolvisser@yahoo.com>
2022-03-07 21:18:59 +0200

    React Routes and Build for Heroku

--- server/views/main.py
+++ server/views/main.py
@@ -2,6 +2,12 @@ from flask import send_from_directory
 from server import app
 
 @app.route('/')
+@app.route('/profile')
+@app.route('/contracts')
+@app.route('/addcontract')
+@app.route('/signup')
+@app.route('/login')
+@app.route('/about')
 def server():
     return send_from_directory(app.static_folder, 'index.html')
     
