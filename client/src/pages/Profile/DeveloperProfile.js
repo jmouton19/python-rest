3627a9f52b510794763041586208c71c539a004b
Phillip <schommarzp@gmail.com>
2022-03-12 11:49:11 +0200

    Small colour change

--- client/src/pages/Profile/DeveloperProfile.js
+++ client/src/pages/Profile/DeveloperProfile.js
@@ -117,7 +117,7 @@ function DeveloperProfile(props) {
 						<Paper elevation={4} sx={{backgroundColor: theme.palette.primary.g2}}>
 							<Stack >
 								{authUser.username == viewUser.username && (
-									<Stack backgroundColor="#388e3c" padding={1} borderRadius= {1}>
+									<Stack backgroundColor={theme.palette.primary.main} padding={1} borderRadius= {1} justifyContent="flex-start">
 										<Typography variant="caption" color={theme.palette.text.light}>
 											Money made:
 										</Typography>
