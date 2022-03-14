3860a175718a3848dc35d52dd31057903bac4a5e
Phillip <schommarzp@gmail.com>
2022-03-14 12:21:42 +0200

    Container around company profile

--- client/src/pages/Profile/CompanyProfile.js
+++ client/src/pages/Profile/CompanyProfile.js
@@ -1,4 +1,4 @@
-import { Avatar, Box, Grid, Paper, Stack, Typography } from "@mui/material";
+import { Avatar, Box, Grid, Paper, Stack, Typography, Container } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { groupByClosed, totalMoney } from "../../utils/contractSorting";
 
@@ -23,70 +23,72 @@ function CompanyProfile(props) {
 	}
 
 	return (
-		<Grid container spacing={2} padding={3}>
-			<Grid item xs={12} md={8}>
-				<Paper elevation={4} sx={{ height: "100%" }}>
-					<Grid
-						container
-						alignItems="center"
-						padding={2}
-						justifyContent="space-between"
-					>
-						<Grid item>
-							<Avatar
-								sx={{
-									width: 100,
-									height: 100,
-									borderRadius: "16px",
-									border: 4,
-								}}
-								src={viewUser.avatarUrl}
-							></Avatar>
+		<Container>
+			<Grid container spacing={2} padding={3}>
+				<Grid item xs={12} md={8}>
+					<Paper elevation={4} sx={{ height: "100%" }}>
+						<Grid
+							container
+							alignItems="center"
+							padding={2}
+							justifyContent="space-between"
+						>
+							<Grid item>
+								<Avatar
+									sx={{
+										width: 100,
+										height: 100,
+										borderRadius: "16px",
+										border: 4,
+									}}
+									src={viewUser.avatarUrl}
+								></Avatar>
+							</Grid>
+							<Grid item>
+								<Stack>
+									<Typography variant="h3">{viewUser.username}</Typography>
+									<Typography variant="h5">{viewUser.email}</Typography>
+									<Typography variant="h6">{viewUser.industry}</Typography>
+								</Stack>
+							</Grid>
 						</Grid>
-						<Grid item>
-							<Stack>
-								<Typography variant="h3">{viewUser.username}</Typography>
-								<Typography variant="h5">{viewUser.email}</Typography>
-								<Typography variant="h6">{viewUser.industry}</Typography>
-							</Stack>
-						</Grid>
-					</Grid>
-				</Paper>
-			</Grid>
-			<Grid item xs={12} md={4}>
-				<Paper elevation={4} sx={{ height: "100%" }}>
-					<Stack sx={{ height: "100%" }}>
-						{authUser.username == viewUser.username && (
-							<Stack backgroundColor="#d32f2f" padding={1} borderRadius={1}>
-								<Typography variant="caption">Money spent:</Typography>
+					</Paper>
+				</Grid>
+				<Grid item xs={12} md={4}>
+					<Paper elevation={4} sx={{ height: "100%" }}>
+						<Stack sx={{ height: "100%" }}>
+							{authUser.username == viewUser.username && (
+								<Stack backgroundColor="#d32f2f" padding={1} borderRadius={1}>
+									<Typography variant="caption">Money spent:</Typography>
+									<Typography variant="h4" align="center">
+										{currencyFormatter.format(moneySpent)}
+									</Typography>
+								</Stack>
+							)}
+							<Stack sx={{ height: "100%" }} padding={1}>
+								<Typography variant="caption" color="error">
+									# Contracts Closed
+								</Typography>
 								<Typography variant="h4" align="center">
-									{currencyFormatter.format(moneySpent)}
+									{numberContractsClosed}
 								</Typography>
 							</Stack>
-						)}
-						<Stack sx={{ height: "100%" }} padding={1}>
-							<Typography variant="caption" color="error">
-								# Contracts Closed
-							</Typography>
-							<Typography variant="h4" align="center">
-								{numberContractsClosed}
-							</Typography>
 						</Stack>
-					</Stack>
-					<Box sx={{ flexGrow: 1 }} />
-				</Paper>
-			</Grid>
-			<Grid item xs={12}>
-				<ContractList
-					condensed
-					method="value"
-					descending={true}
-					viewUser={viewUser}
-					authUser={authUser}
-					status="open"
-				/>
-			</Grid>
-		</Grid>
+						<Box sx={{ flexGrow: 1 }} />
+					</Paper>
+				</Grid>
+				<Grid item xs={12}>
+					<ContractList
+						condensed
+						method="value"
+						descending={true}
+						viewUser={viewUser}
+						authUser={authUser}
+						status="open"
+					/>
+				</Grid>
+			</Grid>	
+		</Container>
 	);
 }
 
